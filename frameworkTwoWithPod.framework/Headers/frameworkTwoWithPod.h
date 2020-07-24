d2d0d8f0d9868c4bc520ce248a3880fefdff6d71
tunb-teko <tu.nb@teko.vn>
2020-07-24 13:15:32 +0700

    Framework two 0.0.1

---
+++ frameworkTwoWithPod.framework/Headers/frameworkTwoWithPod.h
@@ -0,0 +1,19 @@
+//
+//  frameworkTwoWithPod.h
+//  frameworkTwoWithPod
+//
+//  Created by batu on 7/22/20.
+//  Copyright © 2020 Teko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for frameworkTwoWithPod.
+FOUNDATION_EXPORT double frameworkTwoWithPodVersionNumber;
+
+//! Project version string for frameworkTwoWithPod.
+FOUNDATION_EXPORT const unsigned char frameworkTwoWithPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <frameworkTwoWithPod/PublicHeader.h>
+
+
